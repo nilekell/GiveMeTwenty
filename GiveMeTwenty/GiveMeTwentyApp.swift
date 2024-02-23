f75db37102e406bb6225fa280efaeb5e772d7d1a
Nile Kelly <nile@NKs-MacBook-Pro.local>
2024-02-23 18:12:49 +0000

    Initial Commit

---
+++ GiveMeTwenty/GiveMeTwentyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiveMeTwentyApp.swift
+//  GiveMeTwenty
+//
+//  Created by Nile Kelly on 23/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct GiveMeTwentyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
