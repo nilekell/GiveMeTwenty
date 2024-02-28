a190fcc868ee5e39cc5416a636d7b9bab825c313
Nile Kelly <nile@NKs-MacBook-Pro.local>
2024-02-28 16:23:55 +0000

    refactor UserDefaultsPersistenceService to use enums + added default values for setting keys

--- GiveMeTwenty/Services/UserDefaultsPersistenceService.swift
+++ GiveMeTwenty/Services/UserDefaultsPersistenceService.swift
@@ -7,13 +7,53 @@
 
 import Foundation
 
+extension UserDefaults {
+    func valueExists(forKey key: String) -> Bool {
+        return object(forKey: key) != nil
+    }
+}
+
+enum SettingsKeys {
+    static let reminderFrequency = "reminderFrequency"
+    static let runWhenComputerStarts = "runWhenComputerStarts"
+    static let showAppInMenuBar = "showAppInMenuBar"
+    static let popUpMenuMessage = "popUpMenuMessage"
+    static let notificationsEnabled = "notificationsEnabled"
+}
+
 class UserDefaultsPersistenceService {
     
+    // initialising singleton
     static let shared = UserDefaultsPersistenceService()
+    
     private let storage: UserDefaults
 
     private init() {
         storage = UserDefaults.standard
+        
+        // Default values mapped to their keys
+        let defaultValues: [String: Any] = [
+            SettingsKeys.reminderFrequency: 1,
+            SettingsKeys.notificationsEnabled: false,
+            SettingsKeys.runWhenComputerStarts: true,
+            SettingsKeys.showAppInMenuBar: true,
+            SettingsKeys.popUpMenuMessage: "Give Me Twenty!"
+        ]
+        
+        // Iterate over the default values and set them if they don't exist
+        for (key, value) in defaultValues {
+            if !storage.valueExists(forKey: key) {
+                if let boolValue = value as? Bool {
+                    saveBool(key: key, value: boolValue)
+                }
+                if let stringValue = value as? String {
+                    saveString(key: key, value: stringValue)
+                }
+                if let intValue = value as? Int {
+                    saveInt(key: key, value: intValue)
+                }
+            }
+        }
     }
     
     func saveBool(key: String, value: Bool) {
@@ -30,5 +70,13 @@ class UserDefaultsPersistenceService {
     
     func loadString(key: String) -> String {
         storage.string(forKey: key) ?? ""
+    }
+    
+    func saveInt(key: String, value: Int) {
+        storage.set(value, forKey: key)
+    }
+    
+    func loadInt(key: String) -> Int {
+        storage.integer(forKey: key)
     }
 }
