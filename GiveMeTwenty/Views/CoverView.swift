a898c4a1fef46d06a72cd6d825247740b9524db0
Nile Kelly <nile@NKs-MacBook-Pro.local>
2024-03-08 14:32:13 +0000

    wip CoverView

--- GiveMeTwenty/Views/CoverView.swift
+++ GiveMeTwenty/Views/CoverView.swift
@@ -7,9 +7,42 @@
 
 import SwiftUI
 
+struct VisualEffectView: NSViewRepresentable {
+    func makeNSView(context: Context) -> NSVisualEffectView {
+        // called when view is first created
+        let view = NSVisualEffectView()
+
+        view.blendingMode = .behindWindow
+        view.state = .active
+        view.material = .underWindowBackground
+
+        return view
+    }
+
+    func updateNSView(_ nsView: NSVisualEffectView, context: Context) {
+        // called when view state changes
+    }
+}
+
 struct CoverView: View {
+    
+    @AppStorage(SettingsKeys.popUpMenuMessage) var popUpMenuMessage: String = "Time to give me twenty!"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(popUpMenuMessage)
+                .font(.system(size: 64))
+                .bold()
+            
+            Button(action: closeScreen) {
+                Label("Close", systemImage: "arrow.up")
+            }
+        }
+    }
+    
+    
+    func closeScreen() {
+        NSApplication.shared.hide(self)
     }
 }
 
